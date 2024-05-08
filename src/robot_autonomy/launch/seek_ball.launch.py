edbae735c346a1f90558a098b8c0486e7e608958
EliseoNunezVega <eliseo@advanced.farm>
2024-05-08 14:16:18 -0700

    Adds launch file for seek ball autonomy behaviour tree: launches clients/servers as well as detection nodes

---
+++ src/robot_autonomy/launch/seek_ball.launch.py
@@ -0,0 +1,46 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    package_name = 'actions_py'
+
+    find_ball_server = Node(
+            package= package_name,
+            executable= 'find_ball_server',
+            parameters=[],
+         )
+
+    go_to_ball_server = Node(
+            package=package_name,
+            executable='go_to_ball_server',
+            parameters=[],
+         )
+
+    detect_ball = Node(
+        package="get_ball_coordinates",
+        executable="detect",
+        parameters=[],
+    )
+
+    autonomy_node_cmd = Node(
+      package="robot_autonomy",
+      executable="autonomy_node",
+      name="autonomy_node",
+    #   parameters=[{
+    #       "location_file": os.path.join(pkg_tb3_sim, "config", "sim_house_locations.yaml")
+    #   }]
+    )
+
+    return LaunchDescription([
+        find_ball_server,
+        go_to_ball_server,
+        detect_ball,
+        autonomy_node_cmd
+        # twist_stamper
+    ])
